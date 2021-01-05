f8fcd3bd9ca1fff903e6895119d304fe421dcaca
Rodrigo Brechard <rodrigobrechard@gmail.com>
2021-01-05 12:38:14 +0100

    Section 10, extracted search code to a hook and started the flatlist rendering with only title text

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,28 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-    const searchApi = async (searchTerm) => {
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'Madrid'
-                }
-            });
-            setResults(response.data.businesses)    
-            setErrorMessage('');
-        } catch (err) {
-            setErrorMessage('Something went wrong');
-        }
+    const [searchApi, results, errorMessage] = useResults ();
+    const filterResultsByPrice = (price) => {
+        // price === '€' || '€€' || '€€€'
+        return results.filter(results => results.price === price);
     };
 
+    // console.log(results);
     return (
         <View style={styles.background}>
             <SearchBar 
@@ -31,6 +22,9 @@ const SearchScreen = () => {
                 onTermSubmit={() => searchApi(term)}/>
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
+            <ResultsList results={filterResultsByPrice('€')} title="Cost effective"/>
+            <ResultsList results={filterResultsByPrice('€€')} title="Bit Pricier"/>
+            <ResultsList results={filterResultsByPrice('€€€')} title="Spender"/>
         </View>
     )
 };
